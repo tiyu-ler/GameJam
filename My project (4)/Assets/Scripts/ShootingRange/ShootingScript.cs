6d5c64db0322f56ed18cbe0a625a60edae94d7f1
Edodian <pvaleev.dc@gmail.com>
2024-10-26 18:41:18 +0200

    Revert "ShootingRange v2"
    This reverts commit 4bfb7ed1b1eb2ee630e519cb92ff6c4256e151ed.

--- My project (4)/Assets/Scripts/ShootingRange/ShootingScript.cs
+++ My project (4)/Assets/Scripts/ShootingRange/ShootingScript.cs
@@ -6,31 +6,17 @@ public class Shooter : MonoBehaviour
     public float power = 10.0f;
     public AudioClip shootSFX;
     public float DestroyTime;
-    public bool useRigidbody = false, calculateCamDistance = false;
-    public LayerMask hitLayers;
+    public bool useRigidbody = false,calculateCamDistance=false ;
 
     void Update()
     {
+
         if (Input.GetButtonDown("Fire1"))
         {
             if (projectile)
             {
-                Ray ray = new Ray(transform.position, transform.forward);
-                RaycastHit hit;
-
-                if (Physics.Raycast(ray, out hit, Mathf.Infinity, hitLayers))
-                {
-                    Debug.Log("Hit: " + hit.collider.name);
-
-                    if (hit.collider.gameObject.CompareTag("Target"))
-                    {
-                        Destroy(hit.collider.gameObject);
-                    }
-                }
-
-                GameObject newProjectile = Instantiate(projectile, transform.position + transform.forward * (calculateCamDistance ? 1.2f : 0), transform.rotation * Quaternion.Euler(0, 90, 90));
+                GameObject newProjectile = Instantiate(projectile, transform.position + transform.forward*(calculateCamDistance?1.2f:0), transform.rotation*Quaternion.Euler(0, 90, 90));
                 Destroy(newProjectile, DestroyTime);
-
                 if (useRigidbody)
                 {
                     if (!newProjectile.GetComponent<Rigidbody>())
@@ -38,7 +24,6 @@ public class Shooter : MonoBehaviour
 
                     newProjectile.GetComponent<Rigidbody>().AddForce(transform.forward * power, ForceMode.VelocityChange);
                 }
-                
                 if (shootSFX)
                 {
                     if (newProjectile.GetComponent<AudioSource>())
