4bfb7ed1b1eb2ee630e519cb92ff6c4256e151ed
Edodian <pvaleev.dc@gmail.com>
2024-10-26 16:05:41 +0200

    ShootingRange v2

--- My project (4)/Assets/Scripts/ShootingRange/ShootingScript.cs
+++ My project (4)/Assets/Scripts/ShootingRange/ShootingScript.cs
@@ -6,17 +6,31 @@ public class Shooter : MonoBehaviour
     public float power = 10.0f;
     public AudioClip shootSFX;
     public float DestroyTime;
-    public bool useRigidbody = false,calculateCamDistance=false ;
+    public bool useRigidbody = false, calculateCamDistance = false;
+    public LayerMask hitLayers;
 
     void Update()
     {
-
         if (Input.GetButtonDown("Fire1"))
         {
             if (projectile)
             {
-                GameObject newProjectile = Instantiate(projectile, transform.position + transform.forward*(calculateCamDistance?1.2f:0), transform.rotation*Quaternion.Euler(0, 90, 90));
+                Ray ray = new Ray(transform.position, transform.forward);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, Mathf.Infinity, hitLayers))
+                {
+                    Debug.Log("Hit: " + hit.collider.name);
+
+                    if (hit.collider.gameObject.CompareTag("Target"))
+                    {
+                        Destroy(hit.collider.gameObject);
+                    }
+                }
+
+                GameObject newProjectile = Instantiate(projectile, transform.position + transform.forward * (calculateCamDistance ? 1.2f : 0), transform.rotation * Quaternion.Euler(0, 90, 90));
                 Destroy(newProjectile, DestroyTime);
+
                 if (useRigidbody)
                 {
                     if (!newProjectile.GetComponent<Rigidbody>())
@@ -24,6 +38,7 @@ public class Shooter : MonoBehaviour
 
                     newProjectile.GetComponent<Rigidbody>().AddForce(transform.forward * power, ForceMode.VelocityChange);
                 }
+                
                 if (shootSFX)
                 {
                     if (newProjectile.GetComponent<AudioSource>())
