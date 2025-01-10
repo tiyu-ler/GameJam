8fc09d4ad377a56dfe20f5fcec264713d4ee0490
Edodian <pvaleev.dc@gmail.com>
2025-01-10 23:32:14 +0100

    Main Scene Fixes

--- My project (4)/Assets/Scripts/PlayerMovement.cs
+++ My project (4)/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
@@ -11,25 +12,37 @@ public class PlayerMovement : MonoBehaviour
     public stateHandler stateHandler;
     private bool isSFXPlaying = false;
     private bool stepturn;
-
+    public bool inv_x, inv_y;
+    private int invXval, invYval;
+    public string WalkSFXTitle1,WalkSFXTitle2;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+        if (inv_x == true)
+        {
+            invXval = -1;
+        }
+        else { invXval = 1; }
+        if (inv_y == true)
+        {
+            invYval = -1;
+        }
+        else { invYval = 1; }
     }
 
     private void Update()
     {
         if (stateHandler.isPaused == false && stateHandler.isCompleted == false)
         {
-            float moveHorizontal = Input.GetAxis("Horizontal");
-            float moveVertical = Input.GetAxis("Vertical");
+            float moveHorizontal = invXval*Input.GetAxis("Horizontal");
+            float moveVertical = invYval*Input.GetAxis("Vertical");
             movement = new Vector3(moveHorizontal, 0f, moveVertical).normalized;
             animator.SetFloat("walkSpeed", movement.magnitude * moveSpeed);
             if (movement.magnitude > 0)
             {
                 Quaternion targetRotation = Quaternion.LookRotation(movement);
-                if ((Mathf.Abs(moveHorizontal)+Mathf.Abs(moveVertical)) > 0 && !isSFXPlaying)
+                if ((Mathf.Abs(moveHorizontal) + Mathf.Abs(moveVertical)) > 0 && !isSFXPlaying && SoundManager.sndm != null)
                 {
                     StartCoroutine(PlayStep());
                 }
@@ -48,12 +61,12 @@ public class PlayerMovement : MonoBehaviour
 
         if (stepturn == true)
         {
-            FindObjectOfType<SoundManager>().Play("Grass1");
+            FindObjectOfType<SoundManager>().Play(WalkSFXTitle1);
             stepturn = !stepturn;
         }
         else
         {
-            FindObjectOfType<SoundManager>().Play("Grass2");
+            FindObjectOfType<SoundManager>().Play(WalkSFXTitle2);
             stepturn = !stepturn;
         }
         yield return new WaitForSeconds(0.60f);
